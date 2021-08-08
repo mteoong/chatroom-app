b10199aec6d4974d9e3d3b16ef056687d1e56846
Marc Teo Ong <mrc.teoong@gmail.com>
2021-08-08 10:19:19 +0800

    add socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = scoketio(server);
 
 const PORT = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.listen(PORT, () => {
+let count = 0;
+
+io.on('connection', (socket) => {
+    console.log('new websocket connection');
+
+    socket.emit('countUpdated', count);
+    socket.on('increment', ()=> {
+        count++
+        io.emit('countUpdated', count);
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}`);
 })
